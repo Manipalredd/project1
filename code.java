14bc29ddb30a1a30b3e90cab70f2fff69678225b
Manipal reddy <manipalreddy622@gmail.com>
2024-06-10 12:30:27 +0530

    codeAdded

---
+++ code.java
@@ -0,0 +1,15 @@
+class hello
+{
+  public static Void main (String[] args) {
+    int i;
+    int j;
+    for (i=1;i<=5 ;i++) {
+      for(j=1;j<=5;j++)
+      {
+        System.out.print("*");
+        
+      }
+        System.out.println();
+      }
+  }
+  }
